a6e882c04789dae9ee3a87754598a3b032efc92c
Neil Bartlett <njbartlett@gmail.com>
2012-11-01 00:30:45 +0000

    Factored out use of Jersey client library in client repository

--- org.bndtools.rt.repository.rest/src/org/bndtools/rt/repository/client/RemoteRestRepository.java
+++ org.bndtools.rt.repository.rest/src/org/bndtools/rt/repository/client/RemoteRestRepository.java
@@ -3,6 +3,8 @@ package org.bndtools.rt.repository.client;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -11,8 +13,8 @@ import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
 
 import aQute.bnd.deployer.http.DefaultURLConnector;
 import aQute.bnd.deployer.repository.CachingUriResourceHandle;
@@ -22,13 +24,11 @@ import aQute.bnd.service.RegistryPlugin;
 import aQute.bnd.service.RepositoryPlugin;
 import aQute.bnd.service.url.URLConnector;
 import aQute.bnd.version.Version;
+import aQute.lib.io.IO;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 public class RemoteRestRepository implements RepositoryPlugin, RegistryPlugin {
 	
@@ -74,14 +74,38 @@ public class RemoteRestRepository implements RepositoryPlugin, RegistryPlugin {
 	}
 
 	@Override
-	public PutResult put(InputStream stream, PutOptions options) throws Exception {
-		URI postUri = UriBuilder.fromUri(baseUri).path("bundles").build();
-		WebResource resource = Client.create().resource(postUri);
-		ClientResponse response = resource.entity(stream, MediaType.APPLICATION_OCTET_STREAM_TYPE).post(ClientResponse.class);
-		URI location = response.getLocation();
+	public PutResult put(InputStream inputStream, PutOptions options) throws Exception {
+		URI postUri = new URI(baseUri.getScheme(), baseUri.getUserInfo(), baseUri.getHost(), baseUri.getPort(), baseUri.getPath() + "/bundles", null, null);
+		
+		HttpURLConnection conn = (HttpURLConnection) postUri.toURL().openConnection();
+		conn.setRequestProperty(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM);
+		conn.setRequestMethod("POST");
+		conn.setDoOutput(true);
+		
+		// Send the data
+		byte[] buffer = new byte[1024];
+		OutputStream postStream = conn.getOutputStream();
+		try {
+			while (true) {
+				int bytesRead = inputStream.read(buffer, 0, 1024);
+				if (bytesRead < 0) break;
+				postStream.write(buffer, 0, bytesRead);
+			}
+			postStream.flush();
+		} finally {
+			IO.close(postStream);
+		}
+		
+		// Read the response header
+		int response = conn.getResponseCode();
+		if (response < 200 || response >= 300)
+			throw new Exception(String.format("Server returned error code %d", response));
+		String location = conn.getHeaderField("Location");
+		if (location == null)
+			throw new Exception("Server did not return a Location header");
 		
 		PutResult result = new PutResult();
-		result.artifact = location;
+		result.artifact = new URI(location);
 		result.digest = null;
 		
 		return result;
@@ -92,7 +116,7 @@ public class RemoteRestRepository implements RepositoryPlugin, RegistryPlugin {
 		URLConnector connector = registry != null ? registry.getPlugin(URLConnector.class) : null;
 		if (connector == null) connector = new DefaultURLConnector();
 		
-		URI bundleUri = UriBuilder.fromUri(baseUri).path("bundles/{bsn}/{version}").build(bsn, version);
+		URI bundleUri = new URI(baseUri.getScheme(), baseUri.getUserInfo(), baseUri.getHost(), baseUri.getPort(), baseUri.getPath() + "/bundles/" + bsn + "/" + version, null, null);
 		CachingUriResourceHandle handle = new CachingUriResourceHandle(bundleUri, cacheDir, connector, CachingMode.PreferRemote);
 		return handle.request();
 	}
@@ -106,15 +130,18 @@ public class RemoteRestRepository implements RepositoryPlugin, RegistryPlugin {
 	public List<String> list(String pattern) throws Exception {
 		LinkedList<String> result = new LinkedList<String>();
 		
-		URI requestUri = UriBuilder.fromUri(baseUri).path("bundles").queryParam("pattern", pattern).build();
-		WebResource resource = Client.create().resource(requestUri);
-		String response = resource.get(String.class);
+		URI requestUri = new URI(baseUri.getScheme(), baseUri.getUserInfo(), baseUri.getHost(), baseUri.getPort(), baseUri.getPath() + "/bundles", "pattern=" + pattern, null);
+		InputStream stream = requestUri.toURL().openStream();
 		
-		JsonNode rootNode = new ObjectMapper(jsonFactory).readTree(response);
-		Iterable<JsonNode> iterable = rootNode.isArray() ? rootNode : Collections.singletonList(rootNode);
-		for (JsonNode node : iterable) {
-			String bsn = node.get("bsn").asText();
-			result.add(bsn);
+		try {
+			JsonNode rootNode = new ObjectMapper(jsonFactory).readTree(stream);
+			Iterable<JsonNode> iterable = rootNode.isArray() ? rootNode : Collections.singletonList(rootNode);
+			for (JsonNode node : iterable) {
+				String bsn = node.get("bsn").asText();
+				result.add(bsn);
+			}
+		} finally {
+			IO.close(stream);
 		}
 		
 		return result;
@@ -124,17 +151,20 @@ public class RemoteRestRepository implements RepositoryPlugin, RegistryPlugin {
 	public SortedSet<Version> versions(String bsn) throws Exception {
 		SortedSet<Version> result = new TreeSet<Version>();
 		
-		URI requestUri = UriBuilder.fromUri(baseUri).path("bundles/{bsn}").build(bsn);
-		WebResource resource = Client.create().resource(requestUri);
-		String response = resource.get(String.class);
+		URI requestUri = new URI(baseUri.getScheme(), baseUri.getUserInfo(), baseUri.getHost(), baseUri.getPort(), baseUri.getPath() + "/bundles/" + bsn, null, null);
+		InputStream stream = requestUri.toURL().openStream();
 		
-		JsonNode rootNode = new ObjectMapper(jsonFactory).readTree(response);
-		Iterable<JsonNode> iterable = rootNode.isArray() ? rootNode : Collections.singletonList(rootNode);
-		for (JsonNode node : iterable) {
-			String versionStr = node.get("version").asText();
-			Version version = new Version(versionStr);
-			result.add(version);
-		}		
+		try {
+			JsonNode rootNode = new ObjectMapper(jsonFactory).readTree(stream);
+			Iterable<JsonNode> iterable = rootNode.isArray() ? rootNode : Collections.singletonList(rootNode);
+			for (JsonNode node : iterable) {
+				String versionStr = node.get("version").asText();
+				Version version = new Version(versionStr);
+				result.add(version);
+			}
+		} finally {
+			IO.close(stream);
+		}
 		
 		return result;
 	}
