598fadac1357b57bb071a6042be4178291d8200f
Neil Bartlett <njbartlett@gmail.com>
2013-03-27 18:47:45 +0000

    Cleaned up MongoDB packaging more

--- org.bndtools.rt.packager.mongodb/src/org/bndtools/rt/packager/mongodb/guard/MongoProcessGuard.java
+++ org.bndtools.rt.packager.mongodb/src/org/bndtools/rt/packager/mongodb/guard/MongoProcessGuard.java
@@ -20,15 +20,26 @@ import org.bndtools.service.endpoint.Endpoint;
 import org.bndtools.service.mongodb.MongoProperties;
 import org.bndtools.service.packager.ProcessGuard;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.remoteserviceadmin.RemoteConstants;
 
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.ConfigurationPolicy;
 import aQute.bnd.annotation.metatype.Configurable;
 
-@Component(designateFactory = MongoProperties.class, properties = { "type=mongodb" }, name="org.bndtools.rt.packager.mongodb.guard")
+@Component(
+		name = MongoProcessGuard.PID, 
+		designateFactory = MongoProperties.class,
+		configurationPolicy = ConfigurationPolicy.optional,
+		properties = {
+			"type=mongodb",
+			Constants.SERVICE_PID + "=" + MongoProcessGuard.PID
+		})
 public class MongoProcessGuard implements ProcessGuard {
+	
+	static final String PID = "org.bndtools.rt.packager.mongodb.guard";
 	
 	private static final String MONGO_URI_SCHEME = "mongodb";
 
